d9789a87ed393a28ab598a15594c4f2c3905056c
Donnadieu <donnadieu.86@gmail.com>
2018-03-27 16:23:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = 'bfd3285abe6215e1a4076082bbd13fa2c4344929'
+
+fetch('https://api.github.com/Donnadieu/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json);)
